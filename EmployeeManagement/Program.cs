c6300cdac6fd76927f84f658fe56f5bb8954676f
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-10 11:34:16 -0800

    Check in Master Branch

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-            //UC1_EmployeeAttendence.EmployeeAttendence();
-            //UC2_DailyEmployeeWage.EmployeeWage();
-            //UC3_PartTimeEmployeeWage.PartTimeEmployeeWage();
-            //UC4_EmpWageUsingCase.EmpWageUsingSwitch();
-            //UC5_EmpWageFor20Days.EmpWageFor20Days(); 
-              UC6_EmpWageUsingWhileloop.EmpWageFor20Days();
+            
+            UC1_EmployeeAttendence.EmployeeAttendence();
+            UC2_DailyEmployeeWage.EmployeeWage();
+            UC3_PartTimeEmployeeWage.PartTimeEmployeeWage();
+            UC4_EmpWageUsingCase.EmpWageUsingSwitch();
+            UC5_EmpWageFor20Days.EmpWageFor20Days(); 
+            UC6_EmpWageUsingWhileloop.EmpWageFor20Days();
         }
 }
 }
